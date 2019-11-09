efee3f3c7fc56592799a248ee04f98ff4f6ec649
Xun Zhou <segentor@gmail.com>
2019-11-09 13:35:40 +0100

    add bulma

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.scss';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="App section">
+        <h1>Hello CodeSandbox</h1>
+        <h2>Start editing to see some magic happen!</h2>
+        <button className="button is-danger is-outlined">Hello</button>
+      </div>
+    </div>
+  );
+}
+
+export default App;
