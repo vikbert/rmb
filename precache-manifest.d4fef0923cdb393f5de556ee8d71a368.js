94bae8ea302d9b984b71121dbc02e32b8ee7fa5c
Xun Zhou <segentor@gmail.com>
2019-11-16 15:30:48 +0100

    deploy: github gh-pages 2019-11-16 15:30:45

---
+++ precache-manifest.d4fef0923cdb393f5de556ee8d71a368.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "522c8c026633ebda3d6fea7e25103414",
+    "url": "./index.html"
+  },
+  {
+    "revision": "3731878ef95bf6dfac85",
+    "url": "./static/js/2.a05cd9a6.chunk.js"
+  },
+  {
+    "revision": "5cf6c424d03fff052ee4",
+    "url": "./static/js/main.a487adc3.chunk.js"
+  },
+  {
+    "revision": "887dd86317002db178e7",
+    "url": "./static/js/runtime-main.582f52c9.js"
+  },
+  {
+    "revision": "0bf28f8a882f1887ff707abd90167bbc",
+    "url": "./static/media/blooming.0bf28f8a.svg"
+  },
+  {
+    "revision": "a043d666c2bb444d70cdd259572f612a",
+    "url": "./static/media/feedback.a043d666.svg"
+  },
+  {
+    "revision": "6e1f465db055120d65b219296eaed128",
+    "url": "./static/media/fresh-square.6e1f465d.svg"
+  },
+  {
+    "revision": "627d88e8828da969c838f25586437cce",
+    "url": "./static/media/rmb7.627d88e8.png"
+  }
+]);
